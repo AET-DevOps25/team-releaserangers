d7d88069992769cd4f562a81539c7cd2d0c76e91
Luis Leutbecher <luis.leutbecher@tum.de>
2025-05-15 21:40:19 +0200

    Experimental SpringBoot Setup, Add small JWT Demo

---
+++ coursemgmt-service/src/main/java/devops25/releaserangers/coursemgmt_service/CoursemgmtServiceApplication.java
@@ -0,0 +1,13 @@
+package devops25.releaserangers.coursemgmt_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemgmtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemgmtServiceApplication.class, args);
+	}
+
+}
