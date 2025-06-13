8f1eb22d2c54cb80202d08cc92b43e9e6fe5c885
Jonathan Müller <code@jonathanmuller.de>
2025-06-13 14:11:29 +0200

    Remove unused import

--- server/authentication-service/src/main/java/devops25/releaserangers/authentication_service/controller/AuthController.java
+++ server/authentication-service/src/main/java/devops25/releaserangers/authentication_service/controller/AuthController.java
@@ -13,8 +13,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
