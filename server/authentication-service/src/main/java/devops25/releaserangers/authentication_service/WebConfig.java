eb7d8a59e35d6ddf7475bc5b9cb120d672b3556a
Luis Leutbecher <luis.leutbecher@tum.de>
2025-06-13 15:25:27 +0200

    resolve CORS policy violation

--- server/authentication-service/src/main/java/devops25/releaserangers/authentication_service/WebConfig.java
+++ server/authentication-service/src/main/java/devops25/releaserangers/authentication_service/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Allow all origins, TODO replace with specific origins in production
+                        .allowedOrigins("http://localhost:3000", "http://client:3000") // Allow all origins, TODO replace with specific origins in production
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
