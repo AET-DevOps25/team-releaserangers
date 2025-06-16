9c678fb0549cf60deceba651480d7eb8384ac32f
Luis Leutbecher <107487111+LuisL02@users.noreply.github.com>
2025-06-16 15:44:12 +0200

    Fix/GitHub workflow | Add test workflow (for now just for auth service) (#63)
    * Refactor GitHub Actions to use matrix
    
    * Add test workflow
    
    * Add PostgreSQL test container and update tests;
    
    * Add TODOs
    
    * Update .gitignore

--- server/authentication-service/src/test/java/devops25/releaserangers/authentication_service/AuthenticationServiceApplicationTests.java
+++ server/authentication-service/src/test/java/devops25/releaserangers/authentication_service/AuthenticationServiceApplicationTests.java
@@ -1,13 +1,44 @@
 package devops25.releaserangers.authentication_service;
 
+import devops25.releaserangers.authentication_service.service.CustomUserDetailsService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
 @SpringBootTest
 class AuthenticationServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    // TODO, see if we want to use this?
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("test_db")
+            .withUsername("release")
+            .withPassword("ranger");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "none");
+    }
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Test
+    void contextLoads() {
+        // Add your test logic here
+        assertTrue(true);
+    }
 
 }
