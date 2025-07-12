5a9ec4e0b40684d98c0aa4c95f9b65b73d3b587c
Jonathan Müller <code@jonathanmuller.de>
2025-07-12 02:17:28 +0200

    fix endpoints.ts

--- client/src/server/endpoints.ts
+++ client/src/server/endpoints.ts
@@ -16,12 +16,12 @@ const getApiUrl = () => {
   return ""
 }
 
-export const COURSES_ENDPOINT = () => `${getApiUrl()}/courses`
+export const COURSES_ENDPOINT = `${getApiUrl()}/courses`
 export const COURSE_ENDPOINT = (courseId: string) => `${getApiUrl()}/courses/${courseId}`
 export const CHAPTER_ENDPOINT = (chapterId: string) => `${getApiUrl()}/chapters/${chapterId}`
-export const FAVORITES_ENDPOINT = () => `${getApiUrl()}/favorites`
-export const USER_ENDPOINT = () => `${getApiUrl()}/auth/user`
-export const SIGNIN_ENDPOINT = () => `${getApiUrl()}/auth/signin`
-export const SIGNUP_ENDPOINT = () => `${getApiUrl()}/auth/signup`
-export const SIGNOUT_ENDPOINT = () => `${getApiUrl()}/auth/signout`
-export const UPLOAD_ENDPOINT = () => `${getApiUrl()}/upload`
+export const FAVORITES_ENDPOINT = `${getApiUrl()}/favorites`
+export const USER_ENDPOINT = `${getApiUrl()}/auth/user`
+export const SIGNIN_ENDPOINT = `${getApiUrl()}/auth/signin`
+export const SIGNUP_ENDPOINT = `${getApiUrl()}/auth/signup`
+export const SIGNOUT_ENDPOINT = `${getApiUrl()}/auth/signout`
+export const UPLOAD_ENDPOINT = `${getApiUrl()}/upload`
